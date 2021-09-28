61371b96f195822922f4cb49c72454271ed208ed
LyubovShuliak <87687676+LyubovShuliak@users.noreply.github.com>
2021-09-28 16:28:26 +0300

    аыа

--- t00_protomagician/js/script.js
+++ t00_protomagician/js/script.js
@@ -1,7 +1,38 @@
 /* 
   your code
 */
+function Creature(name, age, species){
+this.name = name
+this.age = age
+this.species = species
+this.sayHello=()=>{
+  console.log(`Hello, my name is ${this.name}!`);
+}
 
+}
+
+const creature = new Creature("Linda", 22, "human" )
+
+function Human(job){
+  this.job = job
+  
+  }
+  function Dog(color){
+    this.color = color
+    
+    }
+  
+    function Vampire(title){
+      this.title = title
+      
+      }
+
+      const  dog = Object.assign(new Creature("ff", 3, "ffvf"), new Dog("fsdf"))
+      const human = Object.assign(new Creature("ff", 3, "ffvf"), new Human("fsdf"))
+      const vapire = Object.assign(new Creature("ff", 3, "ffvf"), new Human("fsdf"), new Vampire("ffsf") )
+    console.log(dog);
+    console.log(human);
+    console.log(vapire);
 const magician = {
   _hat: './assets/images/hat.png',
   _getPortrait(){
